fefc75894a2a5490048cfc151ecb119bf6b39025
Enow Mbi <benowmbi@yahoo.com>
2022-01-31 10:20:42 +0100

    Add show, update and edit actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
   def new
     @user = User.new
   end
@@ -12,9 +13,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def show; end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to(user_path(@user), notice: "User successfully updated!")
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user ||= User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'dashboards#show'
   get 'dashboards/show', to: 'dashboards#show'
 
-  resources :users, only: %i[new create show]
+  resources :users, only: %i[new create show edit update]
 end
