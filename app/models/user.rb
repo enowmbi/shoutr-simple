062a4b51c3574b997a1f76b2aae9ee3f70dfd6e5
Enow Mbi <benowmbi@yahoo.com>
2022-01-31 10:05:29 +0100

    Add validation in users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+  EMAIL_FORMAT = /\w+@\w+\.[a-z]{2,3}/
   has_secure_password
+
+  validates :email, format: { with: EMAIL_FORMAT }
+  validates :username, length: { within: 5..13 }
+  validates :password, length: { within: 8..16 }
 end
