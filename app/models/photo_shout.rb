eccff88d9188b42b57ea27c0c9f45798e7502401
Enow Mbi <benowmbi@yahoo.com>
2022-02-02 11:56:25 +0100

    Add action_storage variant

--- app/models/photo_shout.rb
+++ app/models/photo_shout.rb
@@ -3,7 +3,7 @@
 class PhotoShout < ApplicationRecord
   has_many :shouts, as: :shoutable, dependent: :destroy
 
-  has_one_attached :image # do |attachable|
-  # attachable.variant :thumbnail, resize_to_limit: [200, 150]
-  # end
+  has_one_attached :image do |attachable|
+    attachable.variant :thumbnail, resize_to_limit: [200, 150]
+  end
 end
